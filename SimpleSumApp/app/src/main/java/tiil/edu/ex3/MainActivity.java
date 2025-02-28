78394632852b16f2eec0bd923265028d5e2769aa
nguyen-ngoc-trai <trai.nn.64cntt@ntu.edu.vn>
2025-02-28 15:11:45 +0700

    update

--- SimpleSumApp/app/src/main/java/tiil/edu/ex3/MainActivity.java
+++ SimpleSumApp/app/src/main/java/tiil/edu/ex3/MainActivity.java
@@ -1,6 +1,8 @@
 package tiil.edu.ex3;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,28 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    //Đây là bộ lắng nghe và xử lí xử kiện lên nút tính tổng
+    public void XuLiCong(View view){
+        //Tìm, tham chiếu đến đều khiển
+        EditText editTextSoA = findViewById(R.id.editA);
+        EditText editTextSoB = findViewById(R.id.editB);
+        EditText editTexTKetQua = findViewById(R.id.editKQ);
+
+        //Lấy dũ liệu vê
+        String strA = editTextSoA.getText().toString();
+        String strB = editTextSoB.getText().toString();
+
+        //Chuyển dữ liệu sang số
+        int soA = Integer.parseInt(strA);
+        int soB = Integer.parseInt(strB);
+
+        //Tính toán theo yêu cầu
+        int Tong = soA + soB;
+        String strTong = String.valueOf(Tong);
+
+        //Hiện ra kết quả
+        editTexTKetQua.setText(strTong);
     }
 }
