47ffc1e1ab979de4eb8510526de7828db21a8295
nguyen-ngoc-trai <trai.nn.64cntt@ntu.edu.vn>
2025-03-26 10:34:48 +0700

    update

--- RecyclerView/app/src/main/java/tiil/edu/usingrecyclerview/LandScapeAdapter.java
+++ RecyclerView/app/src/main/java/tiil/edu/usingrecyclerview/LandScapeAdapter.java
@@ -1,13 +1,57 @@
 package tiil.edu.usingrecyclerview;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class LandScapeAdapter extends RecyclerView.Adapter< >{
+import java.util.ArrayList;
+import java.util.zip.Inflater;
+
+public class LandScapeAdapter extends RecyclerView.Adapter< LandScapeAdapter.ImtemLandHolder>{
+    Context context;
+    ArrayList<LandScape> listdata;
+
+    public LandScapeAdapter(Context context, ArrayList<LandScape> listdata) {
+        this.context = context;
+        this.listdata = listdata;
+    }
+
+    @NonNull
+    @Override
+    public ImtemLandHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater cai_bom = LayoutInflater.from(context);
+        View vitem= cai_bom.inflate(R.layout.item_land,parent, false);
+        ImtemLandHolder viewholderCreated = new ImtemLandHolder(vitem);
+        return viewholderCreated;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ImtemLandHolder holder, int position) {
+        //lấy đối tượng hiển thị
+        LandScape landScapehienthi = listdata.get(position);
+        //Trích thông tin
+        String caption  =landScapehienthi.getLandCation();
+        String tenanh = landScapehienthi.getLandImageFileName();
+        //Đặt vào các trường thông tin của holder
+        holder.tvCaption.setText(caption);
+        //đặt ảnh
+        String packagetnam = holder.itemView.getContext().getPackageName();
+        int  imageID = holder.itemView.getResources().getIdentifier(tenanh,"mipmap", packagetnam);
+        holder.ivLandScape.setImageResource(imageID);
+    }
+
+    @Override
+    public int getItemCount()
+    {
+        return listdata.size();
+    }
+
     class ImtemLandHolder extends RecyclerView.ViewHolder{
         TextView tvCaption;
         ImageView ivLandScape;
--- RecyclerView/app/src/main/java/tiil/edu/usingrecyclerview/MainActivity.java
+++ RecyclerView/app/src/main/java/tiil/edu/usingrecyclerview/MainActivity.java
@@ -7,8 +7,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    LandScapeAdapter landScapeAdapter;
+    ArrayList<LandScape> recyclerviewdata;
+    RecyclerView recyclerViewLandScape;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +24,23 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        recyclerviewdata = getdataforreyclerview();
+
+        recyclerViewLandScape =findViewById(R.id.recyclerland);
+        RecyclerView.LayoutManager layoutlinear = new LinearLayoutManager(this);
+        recyclerViewLandScape.setLayoutManager(layoutlinear);
+
+        landScapeAdapter =new LandScapeAdapter(this, recyclerviewdata);
+        recyclerViewLandScape.setAdapter(landScapeAdapter);
+
+    }
+    ArrayList<LandScape> getdataforreyclerview(){
+        ArrayList<LandScape> dsdulieu = new ArrayList<>();
+        LandScape landScape1 = new LandScape("nha_trang", "tp nha trang");
+        dsdulieu.add(landScape1);
+        dsdulieu.add(new LandScape("phan_thiet", "tp phan thiet"));
+        dsdulieu.add(new LandScape("da_nang", "tp da nang"));
+        dsdulieu.add(new LandScape("quy_nhon", "tp quy nhon"));
+        return dsdulieu;
     }
 }
