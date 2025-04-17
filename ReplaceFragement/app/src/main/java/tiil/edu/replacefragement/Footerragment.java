adce6f6dfb2a043b8096e43fe3284c4791da8778
nguyen-ngoc-trai <trai.nn.64cntt@ntu.edu.vn>
2025-04-17 10:43:10 +0700

    update

---
+++ ReplaceFragement/app/src/main/java/tiil/edu/replacefragement/Footerragment.java
@@ -0,0 +1,64 @@
+package tiil.edu.replacefragement;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class Footerragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.fragment_footerragment, container, false);
+
+        Button b1 = v.findViewById(R.id.button1);
+        Button b2 = v.findViewById(R.id.button2);
+        Button b3 = v.findViewById(R.id.button3);
+
+        FragmentManager fragmentManager = getParentFragmentManager();
+        // Inflate the layout for this fragment
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView_content, new oneFragment())
+                        .commit();
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView_content, new twoFragment())
+                        .commit();
+            }
+        });
+
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView_content, new threeFragment())
+                        .commit();
+            }
+        });
+
+
+        return v;
+    }
+}
